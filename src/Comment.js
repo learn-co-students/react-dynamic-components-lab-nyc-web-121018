c0aeeaf436a1650dea4ae63a7616ae7a3797af26
diaaanek <dkorongy@gmail.com>
2019-02-18 13:11:59 -0500

    Done.

--- src/Comment.js
+++ src/Comment.js
@@ -1,1 +1,18 @@
-// add Comment component here
+// Comment component
+// import React
+// class extends Component
+// import the class BlogPost
+// expects single prop `this.props.commentText`
+
+
+import React, { Component } from 'react';
+export default class Comment extends React.Component {
+  // single div in render() method
+  render()
+  {
+    // <div> className="comment" attribute
+    return <div className="comment">{this.props.commentText} </div>
+  //  we can unpack variable values directly with JSX by wrapping them in {}
+  }
+
+}
